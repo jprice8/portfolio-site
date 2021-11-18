850540140cb3c9d15613610d02cb4dfc946228d1
Jackson Price <59149601+jprice8@users.noreply.github.com>
2021-11-17 18:49:09 -0600

    Revised resumed.

--- components/Welcome.js
+++ components/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = () => {
     <section className="text-2xl font-light text-gray-600">
       <p>
         See what I&apos;ve been working on below. Or find my
-        <a className="hover:text-red-300 border-b-2 font-bold transition duration-300" href="https://jacksonprice-site-files.s3.us-east-2.amazonaws.com/website/resume_sep_2021.docx">
+        <a className="hover:text-red-300 border-b-2 font-bold transition duration-300" href="/jackson_price_resume.pdf">
           {" "}Resume{" "}
         </a>
         and{" "}
